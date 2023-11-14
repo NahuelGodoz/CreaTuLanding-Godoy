5bc3731d9a07b0df534134fa01408c4222e9b3b2
NahuelGodoz <godoznahuel@gmail.com>
2023-11-13 23:33:19 -0300

    primer comit

---
+++ src/components/ItemCount/itemCount.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const itemCount = () => {
+  
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default itemCount
---
+++ src/components/Logo/logo.jsx
@@ -0,0 +1,8 @@
+import logotipo from '../../assets/maquinas 3d'
+const logo = () => {
+  return (
+    <img src="{logo}" alt="" />
+  )
+}
+
+export default logo
